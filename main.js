c30b47e6d63e2662e31d5b5b6c9ef57bf450e2f7
harsh-kain <harshkain1899@gmail.com>
2023-11-27 00:57:39 +0530

    create some section

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-console.log("kain");
+
+// const init  = () =>{
+//     gsap.registerPlugin(ScrollTrigger);
+
+//     const locoScroll = new LocomotiveScroll({
+//     el: document.querySelector("#main"),
+//     smooth: true
+//     });
+//     locoScroll.on("scroll", ScrollTrigger.update);
+
+//     ScrollTrigger.scrollerProxy("#main", {
+//     scrollTop(value) {
+//         return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+//     },
+//     getBoundingClientRect() {
+//         return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+//     },
+//     pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+//     });
+
+//     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+//     ScrollTrigger.refresh();
+// }
+
+// init();
+
+const wrapper = document.querySelector(".wrapper"),
+  carousel = document.querySelector(".carousel"),
+  images = document.querySelectorAll(".carousel img"),
+  buttons = document.querySelectorAll(".button");
+
+let imageIndex = 1,
+  intervalId;
+
+const autoSlide = () => {
+  intervalId = setInterval(() => slideImage(++imageIndex), 2000);
+};
+autoSlide();
+
+const slideImage = () => {
+  imageIndex = imageIndex === (images.length-1) ? 1 : imageIndex < 1 ? (images.length-1) - 2 : imageIndex;
+
+  carousel.style.transform = `translate(-${imageIndex * 100}%)`;
+};
+
+const updateClick = (e) => {
+  clearInterval(intervalId);
+  imageIndex += e.target.id === "next" ? 1 : -1;
+  slideImage(imageIndex);
+  autoSlide();
+};
+
+buttons.forEach((button) => button.addEventListener("click", updateClick));
+
+wrapper.addEventListener("mouseover", () => clearInterval(intervalId));
+wrapper.addEventListener("mouseleave", autoSlide);
